31a5057cdef0d9bbe4a3eae5c8b45d7c2e49953d
Vitalie Miron <vm@insomniacdesign.com>
2020-09-07 16:27:25 +0300

    Add graphql code-generator

--- src/Components/Launch/index.tsx
+++ src/Components/Launch/index.tsx
@@ -1,33 +1,19 @@
+/* eslint-disable camelcase */
 import React from 'react';
-import { useQuery, gql } from '@apollo/client';
+import { useLaunchQuery } from '@Generated';
 
 interface L {
-  id: number;
+  id: string;
 }
 
-const launchQuery = gql`
-  query launch($id: ID!) {
-    launch(id: $id) {
-      mission_name
-      rocket {
-        rocket_name
-        rocket_type
-      }
-      links {
-        flickr_images
-      }
-    }
-  }
-`;
-
 export const Launch: React.FunctionComponent<L> = ({ id }): JSX.Element => {
-  const { data, loading } = useQuery(launchQuery, { variables: { id } });
+  const { data, loading } = useLaunchQuery({ variables: { id } });
   if (loading || !data) return <div>Loading...</div>;
   return (
     <div>
-      {data.launch.rocket.rocket_name}
-      {data.launch.links.flickr_images.map((item: any) => {
-        return <img src={item} key={item} alt="rocket" />;
+      {data?.launch?.rocket?.rocket_name}
+      {data?.launch?.links?.flickr_images?.map((item) => {
+        return <img src={item!} key={item} alt="rocket" />;
       })}
     </div>
   );
