bffa6f8cd7a6e2f7eb143f9c0dbc6dc4bef01859
Vitalie Miron <vm@insomniacdesign.com>
2020-09-07 12:31:00 +0300

    Add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "trailingComma": "es5",
+  "singleQuote": true,
+  "printWidth": 80,
+  "editor.formatOnSave": true,
+  "proseWrap": "always",
+  "tabWidth": 2,
+  "requireConfig": false,
+  "useTabs": false,
+  "trailingComma": "none",
+  "bracketSpacing": true,
+  "jsxBracketSameLine": false,
+  "semi": true
+};
+
