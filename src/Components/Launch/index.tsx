4996339ea4f45b7685ec7309e1974a4e04ba5926
Vitalie Miron <vm@insomniacdesign.com>
2020-09-07 14:27:59 +0300

    Add alt-text

--- src/Components/Launch/index.tsx
+++ src/Components/Launch/index.tsx
@@ -23,12 +23,11 @@ const launchQuery = gql`
 export const Launch: React.FunctionComponent<L> = ({ id }): JSX.Element => {
   const { data, loading } = useQuery(launchQuery, { variables: { id } });
   if (loading || !data) return <div>Loading...</div>;
-  console.log(data);
   return (
     <div>
       {data.launch.rocket.rocket_name}
       {data.launch.links.flickr_images.map((item: any) => {
-        return <img src={item} key={item} />;
+        return <img src={item} key={item} alt="rocket" />;
       })}
     </div>
   );
