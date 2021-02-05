0390a76b2af454ebf15164efb6530f50eb116094
Vitalie Miron <vm@insomniacdesign.com>
2021-02-05 10:49:53 +0200

    Add support for jest tests

---
+++ test/jest.setup.ts
@@ -0,0 +1,7 @@
+// #region Global Imports
+import dotenv from 'dotenv';
+import 'jest-styled-components';
+import '@testing-library/jest-dom';
+// #endregion Global Imports
+
+dotenv.config({ path: '/renderer/.env.test' });
